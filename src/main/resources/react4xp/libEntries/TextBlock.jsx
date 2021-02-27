0fa6bd793413ce65521a552f6c670336273fff01
Benny Thomas <jan.thomas@speilbildet.com>
2021-02-27 15:07:50 +0100

    feat(parts): add text block
    add text blokk with H2 title

---
+++ src/main/resources/react4xp/libEntries/TextBlock.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const TextBlock = ({
+  text = '',
+  title = '',
+  titleColor = '',
+}) => (
+  <>
+    { (title || text) && (
+      <div className="text-block">
+        { title && (
+          <div className={ `text-block-title ${titleColor}`}>
+            <h2>{ title }</h2>
+          </div>          
+        )}
+        { text && (
+          <div className="text-block-text">
+            <div className="page-content rich-text" dangerouslySetInnerHTML={{ __html: text }} />
+          </div>      
+        )}
+      </div>      
+    )}
+  </>
+);
+
+TextBlock.propTypes = {
+  /** Plain or html formated text */
+  text: PropTypes.string,
+  
+  /** H2 title */
+  title: PropTypes.string,
+  
+  /** class to set color on title */
+  titleColor: PropTypes.string,
+};
+
+TextBlock.defaultProps = {
+  text: '',
+  title: '',
+  titleCenter: '',
+};
+
+export default TextBlock;
+
+export {
+  TextBlock,
+};
---
+++ src/main/resources/site/parts/textblock/textblock.es6
@@ -0,0 +1,29 @@
+const portal = require('/lib/xp/portal');
+const React4xp = require('/lib/enonic/react4xp');
+
+const { processHtml } = require('../shared/html');
+
+exports.get = function(request) {
+    const content = portal.getContent();
+    const component = portal.getComponent();
+
+    const {
+      config: {
+        title = '',
+        titleColor = '',
+        text = '',
+      } = {},
+    } = component;
+
+    log.info(JSON.stringify(content, null, 4));
+
+    const props = {
+      title,
+      titleColor,
+      text: processHtml(text),
+    };
+
+    log.info(JSON.stringify(props, null, 4));
+
+    return React4xp.render('TextBlock', props, request, { clientRender: true });
+};
