970242a437357f283b43d19ab5cd3d6eec094232
Benny Thomas <jan.thomas@speilbildet.com>
2021-04-10 21:41:35 +0200

    fix: make programme responsive

---
+++ src/stories/parts/ProgrammeMain.stories.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+import { ProgrammeMain } from '../../main/resources/react4xp/libEntries/ProgrammeMain';
+
+import { SingleLayout } from '../layouts';
+
+import '../../main/resources/assets/styles/main.scss';
+
+export default {
+  title: 'Parts/ProgrammeMain',
+  component: ProgrammeMain,
+};
+
+const Template = () => (
+  <SingleLayout>
+		<div data-portal-component-type="part" id="_97587848">
+      <div class="page-content">
+        <div class="programme-main">
+          <div class="programme-main-title">
+            <h1 title="Fylkesprogram" id="Fylkesprogram">Fylkesprogram</h1>
+          </div>
+          <div class="programme-main-sections">
+            <div class="">
+              <div class="programme-section">
+                <div class="programme-section-title">
+                  <h2 id="Fylkesprogram-Innledning" title="Innledning">Innledning</h2>
+                </div>
+                <div class="programme-section-description">
+                  <p class="Standard">
+                    Liberalistene skal male regionen lilla. Desentralisering av makt og innflytelse er grunnleggende. Det betyr stadig mer frihet til den enkelte borger og mindre innflytelse til politiske og byråkratiske organer. Den viktigste oppgaven for Liberalistenes regionspolitikere blir å redusere fylkeskommunens arbeidsoppgaver, alternativt få flere arbeidsoppgaver fra staten, hvis det kan bidra til å redusere statens omfang.
+                  </p>
+
+	                 <p class="Standard">
+                    Liberalistene vil konsekvent arbeide for det prinsippet at beslutninger skal fattes nærmest mulig individet, dersom ikke individet selv tillates å fatte beslutningen. Derfor ser vi positivt på regionalisering av Norge. Dersom det politiske Norge lykkes med å redusere det offentliges oppgaver vesentlig kan vi være positive til å fjerne regionsleddet i forvaltningen. Men det gir ingen mening å fordele regionenes ansatte og arbeidsoppgaver på staten og kommunene og så fjerne regionene. Oppgavene må fjernes permanent fra offentlig sektor og det er useriøst å late som om det er noe å spare på å fjerne et forvaltningsnivå når man mener staten skal ha samme størrelse og vokse videre i samme takt som før.
+                  </p>
+                  
+                  <ul>
+                  	<li class="Standard">
+                      Liberalistene mener det offentlige ikke bør drifte utdanningsinstitusjoner, men overlate videregående skoler til det sivile marked
+                    </li>
+                  	<li class="Standard">
+                      Liberalistene vil arbeide for at pengene skal følge den enkelte elev, dermed vil gode skoler få mange elever og godt skussmål
+                    </li>
+                  	<li class="Standard">
+                      Liberalistene mener det skal være opp til hver enkelt skoleeier å avgjøre hvilke regler som skal gjelde for fravær, så lenge fylkeskommunen er skoleeier bør avgjørelsen om fraværsregler tas av den enkelte rektor. Hvis mange elever ser seg tjent med «frivillig tvang» som gir strenge regler, vil disse skolene bli populære og omvendt.
+                    </li>
+                  	<li class="Standard">
+                      Liberalistene ser det ikke som negativt med «drop outs» fra videregående skoler. Det negative er måten disse behandles av det offentlige i ettertid, som gjør dem uegnet for arbeidsmarkedet ved at de lever på offentlig bistand i årevis.
+                    </li>
+                  	<li class="Standard">
+                      Liberalistene anser lærere og lektorer som ekspertene på pedagogikk og skole. Politikere og administrasjonen i regionskommunen skal i minst mulig grad blande seg inn i deres arbeidshverdag med krav om rapporteringsplikt etc. Det ansvaret skal den enkelte rektor ha, i samråd med den enkelte skoles styre og pedagogiske stab
+                    </li>
+                  </ul>
+
+	                <p class="Standard">
+                    Liberalistene har stor tro på samarbeid og solidariske løsninger, men ikke når de er skattefinansierte. Vi mener at frivillighet er det eneste gode utgangspunkt for fellesskap, fordi det utløser mer ressurser, stor velvilje og kreativ tilnærming til enhver aktivitet.
+                  </p>
+
+                  <p class="Standard">
+                    Liberalistene ser flere mulige fordeler med regionsreformen, men er åpen for å splitte disse opp i mindre regioner, hvis dette er et ønske som tydelig kommer til uttrykk i befolkningen. Vi er skeptiske til den nye regionen Finnmark-Troms, som vi mener får for stor geografisk utstrekning.
+                  </p>
+	              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </SingleLayout>
+);
+
+export const Default = Template.bind({});
+Default.args = {};
